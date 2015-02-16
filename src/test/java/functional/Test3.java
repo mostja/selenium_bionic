2f2a92d1cb57027764dea0296ab853c0165966a3
Abilous <andbilous@gmail.com>
2015-02-16 21:32:32 +0200

    Lesson4

--- src/test/java/functional/Test3.java
+++ src/test/java/functional/Test3.java
@@ -1,15 +1,58 @@
 package functional;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.testng.reporters.Files.readFile;
 
 /**
  * Created by c2614 on 16.02.2015.
  */
 public class Test3 {
 
-    @Test
-    public void someTests(){
-        Assert.assertTrue(true);
+    @BeforeTest
+    public void createFile(){
+        PrintWriter writer = null;
+        try{
+            writer = new PrintWriter("Test.txt", "UTF-8");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        writer.println("Ahalay mahalay 12.39");
+        writer.close();
+
     }
+    @Test
+    public void someTests() throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("Test.txt"));
+        String actual ="";
+        String expected = ".*\\d+(\\.\\d+).*";
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            actual = sb.toString();
+        } finally {
+            br.close();
+        }
+        Assert.assertTrue(actual.matches(expected));
+    }
+    @AfterTest
+    public boolean deleteFile(){
+        File file = new File("Test.txt");
+        return file.delete();
+    }
+
 }
