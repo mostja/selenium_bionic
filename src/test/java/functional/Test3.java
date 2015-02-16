8101a9f29cc23bf3a54b5ada5a4cd7c16a5bcc2a
Abilous <andbilous@gmail.com>
2015-02-16 20:36:13 +0200

    Added Testng.xml and updated pom.xml

---
+++ src/test/java/functional/Test3.java
@@ -0,0 +1,15 @@
+package functional;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by c2614 on 16.02.2015.
+ */
+public class Test3 {
+
+    @Test
+    public void someTests(){
+        Assert.assertTrue(true);
+    }
+}
