96859db9d086539e2764ef8e936be882efba5fe8
Abilous <andbilous@gmail.com>
2015-02-23 21:11:02 +0200

    Added locator final

--- src/test/java/pages/ChildPage.java
+++ src/test/java/pages/ChildPage.java
@@ -7,13 +7,15 @@ import org.openqa.selenium.WebDriver;
  * Created by c2614 on 23.02.2015.
  */
 public class ChildPage extends AbstractPage{
+    private static final By OPEN_CHILDREN_DRESS_LINK = By.id("cat-539");
+
     public ChildPage(WebDriver driver){super(driver);}
 
     public void openOdezhda(){
-        driver.findElement(By.id("cat-539")).click();
+        driver.findElement(OPEN_CHILDREN_DRESS_LINK).click();
     }
 
     public boolean isOpened(){
-        return driver.findElement(By.id("cat-539")).isDisplayed();
+        return driver.findElement(OPEN_CHILDREN_DRESS_LINK).isDisplayed();
     }
 }
